a31eb7455299460381d62d6ecb5d64dad73108ce
Sabrina Silva <102616948+sabrina702@users.noreply.github.com>
2025-08-21 08:19:01 -0300

    Update carregandoRecurso.dart

--- carregandoRecurso.dart
+++ carregandoRecurso.dart
@@ -4,26 +4,20 @@ void main() {
   RandomAccessFile? arquivo;
 
   try {
-    // Abrindo o arquivo para leitura (precisa fechar manualmente)
     arquivo = File('exemplo.txt').openSync();
     print('Arquivo aberto.');
 
-    // Lendo conteúdo como bytes
     List<int> bytes = arquivo.readSync(arquivo.lengthSync());
     String conteudo = String.fromCharCodes(bytes);
-    print('Conteúdo do arquivo: $conteudo');
-
-    // Gerando um erro proposital para mostrar que finally ainda roda
-    //throw Exception('Erro simulado!');
+    print('Conteúdo do arquivo: \n$conteudo');
 
   } catch (e) {
     print('Ocorreu um erro: $e');
 
   } finally {
-    // finally garante que o arquivo seja fechado
     if (arquivo != null) {
       arquivo.closeSync();
       print('Arquivo fechado no finally.');
-    }
+    }else print('Finally executou!');
   }
 }
